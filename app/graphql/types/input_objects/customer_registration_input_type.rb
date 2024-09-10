af31ca1b6ffe42fcbf8aa079386409b5f24ea349
aayush <aayushkhatiwada892@gmail.com>
2024-09-10 11:19:21 +0000

    feature: added customer mutations and serivces

--- app/graphql/types/input_objects/customer_registration_input_type.rb
+++ app/graphql/types/input_objects/customer_registration_input_type.rb
@@ -2,6 +2,10 @@ module Types
   module InputObjects
     class CustomerRegistrationInputType < BaseInputObject
       argument :name, String, required: true
+      argument :phone_no, String, required: true
+      argument :email, String, required: true
+      argument :zipcode, Integer, required: true
+      argument :address, String, required: true
     end
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :organization, through: :memberships
 
   has_many :customer_branches, dependent: :destroy
