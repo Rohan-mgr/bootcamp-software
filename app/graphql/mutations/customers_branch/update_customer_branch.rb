dc2711d020b483d6ba00ea74ecb8b250112b22a1
aayush <aayushkhatiwada892@gmail.com>
2024-09-11 10:32:36 +0000

    fix: customer-branch-mutation-update

--- app/graphql/mutations/customers_branch/update_customer_branch.rb
+++ app/graphql/mutations/customers_branch/update_customer_branch.rb
@@ -2,13 +2,14 @@ module Mutations
   module CustomersBranch
     class UpdateCustomerBranch < BaseMutation
       argument :branch_info, Types::InputObjects::CustomerBranchInputType, required: true
+      argument :id, ID, required: true
 
       field :customer_branch, Types::CustomerBranch::CustomerBranchType, null: true
       field :errors, [ String ], null: true
 
-      def resolve(branch_info: {})
+      def resolve(id:, branch_info: {})
         begin
-          customer_branch_service = ::CustomerBranches::CustomerBranchService.new(branch_info.to_h).execute_branch_updation
+          customer_branch_service = ::CustomerBranches::CustomerBranchService.new(branch_info.to_h.merge(id: id, current_user: context[:current_user])).execute_branch_updation
           if customer_branch_service.success?
             {
               customer_branch: customer_branch_service.branch,
@@ -17,7 +18,7 @@ module Mutations
           else
             {
              customer_branch: nil,
-             errors: customer_branch_service.errors
+             errors: [ customer_branch_service.errors ]
             }
           end
         rescue GraphQL::ExecutionError => err
--- app/graphql/types/input_objects/customer_branch_input_type.rb
+++ app/graphql/types/input_objects/customer_branch_input_type.rb
@@ -4,7 +4,6 @@ module Types
       argument :name, String, required: true
       argument :location, String, required: true
       argument :customer_id, ID, required: true
-      argument :customer_branch_id, ID, required: false
     end
   end
 end
