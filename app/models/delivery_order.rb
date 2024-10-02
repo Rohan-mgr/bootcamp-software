25bca18df03fbb32edcb685e516c5f17db80dfd5
Rohan-mgr <rohan.magar.415@gmail.com>
2024-10-02 15:39:26 +0000

    fix: solved order update issue

--- app/models/delivery_order.rb
+++ app/models/delivery_order.rb
@@ -3,7 +3,7 @@ class DeliveryOrder < ApplicationRecord
   belongs_to :customer_branch, -> { with_deleted }
   belongs_to :asset, optional: true
 
-  belongs_to :order_group, optional: true
+  belongs_to :order_group
   has_many :line_items, -> { with_deleted }, dependent: :destroy
   accepts_nested_attributes_for :line_items, allow_destroy: true
 
